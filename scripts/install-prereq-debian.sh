49943e62d18c327d643aa3e4e95c132a4e4e03dd
Mike McLaughlin <mike@aviatrix.com>
2017-12-21 17:06:30 -0600

    add symlink to aviatrix-demo script

--- scripts/install-prereq-debian.sh
+++ scripts/install-prereq-debian.sh
@@ -105,3 +105,5 @@ printf "\n**********************************************************************
 EOF
 sudo chmod +x /etc/update-motd.d/10-aviatrix-demo-text
 sudo rm -f /etc/update-motd.d/90-updates-available /etc/update-motd.d/91-release-upgrade /etc/update-motd.d/10-help-text /etc/update-motd.d/51-cloudguest
+
+sudo ln -s /etc/update-motd.d/10-aviatrix-demo-text /bin/aviatrix-demo
